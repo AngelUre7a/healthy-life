cfb9c3c07ceeaec578476f0c59523286635bb789
AngelUreña <129628027+AngelUre7a@users.noreply.github.com>
2025-07-31 00:03:37 -0600

    Initialize Commit

---
+++ src/main/java/org/una/progra3/healthy_life/HealthyLifeApplication.java
@@ -0,0 +1,13 @@
+package org.una.progra3.healthy_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/una/progra3/healthy_life/HealthyLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.progra3.healthy_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
