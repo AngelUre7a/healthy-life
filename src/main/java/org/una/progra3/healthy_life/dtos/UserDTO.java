6945d2cc5e50969936ade8fe6082c760298e687a
SaulDc1275 <saul.chinchilla.badilla@est.una.ac.cr>
2025-08-16 02:24:26 -0600

    add(UserDto): se agrega el Dto con su carpeta

---
+++ src/main/java/org/una/progra3/healthy_life/dtos/UserDTO.java
@@ -0,0 +1,83 @@
+
+package org.una.progra3.healthy_life.dtos;
+
+import java.math.BigDecimal;
+import java.util.Set;
+import java.util.List;
+
+public class UserDTO {
+    private BigDecimal id;
+    private String name;
+    private String email;
+    private Long roleId;
+    private Set<Long> favoriteHabitIds;
+    private List<Long> routineIds;
+    private List<Long> progressLogIds;
+    private List<Long> reminderIds;
+    private List<Long> authTokenIds;
+
+    public UserDTO() {}
+
+    public BigDecimal getId() {
+        return id;
+    }
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public Set<Long> getFavoriteHabitIds() {
+        return favoriteHabitIds;
+    }
+    public void setFavoriteHabitIds(Set<Long> favoriteHabitIds) {
+        this.favoriteHabitIds = favoriteHabitIds;
+    }
+
+    public List<Long> getRoutineIds() {
+        return routineIds;
+    }
+    public void setRoutineIds(List<Long> routineIds) {
+        this.routineIds = routineIds;
+    }
+
+    public List<Long> getProgressLogIds() {
+        return progressLogIds;
+    }
+    public void setProgressLogIds(List<Long> progressLogIds) {
+        this.progressLogIds = progressLogIds;
+    }
+
+    public List<Long> getReminderIds() {
+        return reminderIds;
+    }
+    public void setReminderIds(List<Long> reminderIds) {
+        this.reminderIds = reminderIds;
+    }
+
+    public List<Long> getAuthTokenIds() {
+        return authTokenIds;
+    }
+    public void setAuthTokenIds(List<Long> authTokenIds) {
+        this.authTokenIds = authTokenIds;
+    }
+}
